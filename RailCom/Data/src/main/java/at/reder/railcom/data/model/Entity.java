b80caf9ebcd980adbb7b61e550d9c1fbb5a53e8f
alex <alexander.reder@gmail.com>
2018-04-09 20:33:34 +0200

    Refactoring

---
+++ RailCom/Data/src/main/java/at/reder/railcom/data/model/Entity.java
@@ -0,0 +1,13 @@
+package at.reder.railcom.data.model;
+
+/**
+ *
+ * @author alex
+ */
+public interface Entity {
+
+    public String getName();
+    public void setName(String name);
+    public String getDescription();
+    public void setDescription(String description);
+}
